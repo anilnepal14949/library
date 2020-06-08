82879f13fc60f5a4b0b043e9bddea7072e886739
Anil Nepal <anilnepal14949@gmail.com>
2020-06-08 11:59:54 +1000

    added author post route and books delete route

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 
 Route::post('/books','BooksController@store');
 Route::patch('/books/{book}','BooksController@update');
+
+Route::delete('/books/{book}', 'BooksController@destroy');
+
+Route::post('/author','AuthorController@store');
